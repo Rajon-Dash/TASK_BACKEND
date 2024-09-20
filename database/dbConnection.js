149bb9cd33db54f633f31dee57641029885600e1
Rajon Dash <cse_1922020022@lus.ac.bd>
2024-09-20 06:06:00 +0600

    3rd

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose.connect(process.env.MONGO_URL, {
-      dbName: "new_project",
+      dbName: "NEW_PROJECT",
     })
     .then(() => {
       console.log("Connected to database");
     })
-    .catch((error) => {
-      console.log(`Some error occured while connecting to database:${error}`);
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database:${err}`);
     });
 };
